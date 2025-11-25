7d7f118f83b32f5cbbcf24f0a3f780ffb74669d4
Anytiaa <anitachiu0816@gmail.com>
2025-11-25 14:36:10 +0800

    Update p5.cpp

--- src/p5.cpp
+++ src/p5.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-#include <cmath>
-
 using namespace std;
 
-void move(int from, int to) {
-    if ((from == 1 && to == 3) || (from == 3 && to == 1)) return;
-    cout << "Move disk " << "from">> from << " to " << to << "\n";
+void moveDisk(int disk, int from, int to) {
+    if ((from == 1 && to == 3) || (from == 3 && to == 1))
+        return;
+    cout << "Move disk " << disk << " from " << from << " to " << to << "\n";
 }
 
 void hanoi(int n, int from, int to, int aux) {
     if (n == 0) return;
+
     if (from == 1 && to == 3) {
         hanoi(n, 1, 2, 3);
         hanoi(n, 2, 3, 1);
         return;
     }
+
     if (from == 3 && to == 1) {
         hanoi(n, 3, 2, 1);
         hanoi(n, 2, 1, 3);
@@ -22,7 +23,7 @@ void hanoi(int n, int from, int to, int aux) {
     }
 
     hanoi(n - 1, from, aux, to);
-    move(from, to);
+    moveDisk(n, from, to);  // 加入 disk 編號
     hanoi(n - 1, aux, to, from);
 }
 
