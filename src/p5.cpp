1651838675d54cce63db30325b7228285279a728
Anytiaa <anitachiu0816@gmail.com>
2025-11-25 14:26:35 +0800

    Update p5.cpp

--- src/p5.cpp
+++ src/p5.cpp
@@ -3,9 +3,31 @@
 
 using namespace std;
 
+void move(int from, int to) {
+    if ((from == 1 && to == 3) || (from == 3 && to == 1)) return;
+    cout << "Move disk " << "from">> from << " to " << to << "\n";
+}
+
+void hanoi(int n, int from, int to, int aux) {
+    if (n == 0) return;
+    if (from == 1 && to == 3) {
+        hanoi(n, 1, 2, 3);
+        hanoi(n, 2, 3, 1);
+        return;
+    }
+    if (from == 3 && to == 1) {
+        hanoi(n, 3, 2, 1);
+        hanoi(n, 2, 1, 3);
+        return;
+    }
+
+    hanoi(n - 1, from, aux, to);
+    move(from, to);
+    hanoi(n - 1, aux, to, from);
+}
 
 int main() {
-    
-
-    return 0;
+    int n;
+    cin >> n;
+    hanoi(n, 1, 3, 2);
 }
