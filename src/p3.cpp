a0151e0ab5bbe88d985f656c86d8fdff50b73935
Anytiaa <anitachiu0816@gmail.com>
2025-11-25 14:01:00 +0800

    Update p3.cpp

--- src/p3.cpp
+++ src/p3.cpp
@@ -3,8 +3,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    
-    
+bool isPrime(int n){
+    if(n <= 1) return false;
+    if(n == 2) return true;
+    if(n % 2 == 0) return false;
+
+    for(int i = 3;i * i <= n; i+= 2){
+        if(n % i == 0) return false;
+    }
+    return true;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    for(int a = 2;a <= n / 2;a++){
+       int b = n - a;
+       if(isPrime(a) && isPrime(b)){
+        cout << a << ' ' << b << endl;
+        break;
+       }
+    }
     return 0;
 }
