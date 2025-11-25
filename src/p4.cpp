f0ecfff74b0f1138bc0af31915a992612b52b597
Anytiaa <anitachiu0816@gmail.com>
2025-11-25 14:18:36 +0800

    Update p4.cpp

--- src/p4.cpp
+++ src/p4.cpp
@@ -3,8 +3,34 @@
 #include <iostream>
 using namespace std;
 
+void line(int depth){
+    if(depth == 0) return;
+    cout << "|--" << endl;
+    line(depth - 1);
+}
+
+int fib(int n,int depth){
+    line(depth);
+    cout << "SEARCH fib(" << n << ")\n";
+    if (n == 1 || n == 2) {
+        line(depth);
+        cout << "GET fib(" << n << ") = 1\n";
+        return 1;
+    }
+    int x = fib(n - 1, depth + 1);
+    int y = fib(n - 2, depth + 1);
+    int result = x + y;
+
+      line(depth);
+    cout << "GET fib(" << n << ") = " << result << "\n";
+
+    return result;
+}
+
 int main() {
-    
-    
-    return 0;
+    int n;
+    cin >> n;
+
+    int ans = fib(n, 0);
+    cout << ans << "\n";
 }
